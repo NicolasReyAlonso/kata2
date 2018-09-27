af8d4c1ea549e310030856ba0dd3bd2efcdfd8fc
nicolasreyalonso <nicolasreyalonso@192.168.1.37>
2018-09-27 10:34:17 +0100

    V2

--- src/kata/pkg2/Kata2.java
+++ src/kata/pkg2/Kata2.java
@@ -27,16 +27,10 @@ public class Kata2 {
             data[i] =(int) (Math.random()*((20-0)+1))+0;
             
         }
-        Iterator iter = histogram.entrySet().iterator();
-
-        for (int i = 0; i < data.length; i++) {
-            if(histogram.containsKey(data[i])){
-                histogram.put(data[i], histogram.get(data[i])+1);
-            }else{
-                histogram.put(data[i], 1);
-            }
+   
+        for (Integer key : data) {
+            histogram.put(key, histogram.containsKey(key) ? histogram.get(key) + 1 : 1);
         }
-
         for (Map.Entry<Integer, Integer> entry : histogram.entrySet()) {
         System.out.println("Key = " + entry.getKey() + ", Value = " + entry.getValue()); }
     }
